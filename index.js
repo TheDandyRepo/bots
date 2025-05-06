2e252e3edf07e21522394bd194dcfcc806ad9d30
Gigi’s Exploits <kbrattysssk@gmail.com>
2025-05-06 20:13:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,11 @@ client.once('ready', () => {
 });
 
 client.login(process.env.TOKEN);
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
